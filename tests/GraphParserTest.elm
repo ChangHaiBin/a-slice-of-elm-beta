b521ee3933927921a6c4b35b7348a0eee86654cd
Mads Flensted-Urech <mads@coadu.com>
2018-03-14 17:53:53 +0800

    Prepare slice-tests-small

--- tests/GraphParserTest.elm
+++ tests/GraphParserTest.elm
@@ -15,19 +15,4 @@ suite =
             \_ ->
                 Expect.equal (parse "A: Kevin")
                     [ Ok (N "A" "Kevin") ]
-        , test "parse label with space" <|
-            \_ ->
-                Expect.equal (parse "A : Kevin Spacey")
-                    [ Ok (N "A" "Kevin Spacey") ]
-        , test "parse edge" <|
-            \_ ->
-                Expect.equal (parse "A -> B : worked with")
-                    [ Ok (E "A" "B" "worked with") ]
-        , test "parse edge and node" <|
-            \_ ->
-                Expect.equal (parse "A:foo\nB : bar\nA -> B : worked with")
-                    [ Ok (N "A" "foo")
-                    , Ok (N "B" "bar")
-                    , Ok (E "A" "B" "worked with")
-                    ]
         ]
