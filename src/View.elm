572d68b215175e49650f501aec9379124f33c30c
Mads Flensted-Urech <mads@coadu.com>
2018-03-14 18:26:23 +0800

    Prepare slice-view-svg-medium

--- src/View.elm
+++ src/View.elm
@@ -7,8 +7,6 @@ import Bootstrap.Grid.Row as Row
 import GraphParser
 import Html
 import Html.Attributes as HA
-import Svg exposing (..)
-import Svg.Attributes as SA
 import Types exposing (..)
 
 
@@ -35,12 +33,12 @@ view { editor, graph } =
         [ Grid.row []
             [ Grid.col []
                 [ Html.h1 []
-                    [ text "A slice of Elm "
-                    , Html.small [ HA.attribute "class" "text-muted" ] [ Html.em [] [ text "Beta" ] ]
+                    [ Html.text "A slice of Elm "
+                    , Html.small [ HA.attribute "class" "text-muted" ] [ Html.em [] [ Html.text "Beta" ] ]
                     ]
                 , Html.p []
-                    [ text "Specify a graph in the left pane, and see the result in the right. The layout is calculated with the external JS library"
-                    , Html.a [ HA.href "https://github.com/dagrejs/dagre/wiki" ] [ text " dagre.js." ]
+                    [ Html.text "Specify a graph in the left pane, and see the result in the right. The layout is calculated with the external JS library"
+                    , Html.a [ HA.href "https://github.com/dagrejs/dagre/wiki" ] [ Html.text " dagre.js." ]
                     ]
                 ]
             ]
@@ -58,7 +56,7 @@ dataPane : String -> Bool -> Html.Html Msg
 dataPane editor hasErrors =
     Form.form []
         [ Form.group []
-            [ Form.label [] [ text "Graph specification:" ]
+            [ Form.label [] [ Html.text "Graph specification:" ]
             , Textarea.textarea
                 ([ Textarea.id "data"
                  , Textarea.rows 20
@@ -79,7 +77,7 @@ errorPane : List String -> Html.Html Msg
 errorPane errors =
     Form.form []
         [ Form.group []
-            [ Form.label [] [ text "Parse errors:" ]
+            [ Form.label [] [ Html.text "Parse errors:" ]
             , Textarea.textarea
                 [ Textarea.id "data"
                 , Textarea.rows 20
@@ -98,116 +96,22 @@ errorPane errors =
 graphPane : Maybe GraphData -> Html.Html msg
 graphPane graph =
     let
-        defaultGraph =
-            GraphData defaultGraphOptions defaultGraphValues [] []
+        numNodes =
+            graph
+                |> Maybe.map (List.length << .nodes)
+                |> Maybe.withDefault 0
 
-        graphData =
-            Maybe.withDefault defaultGraph graph
-
-        sw =
-            toString graphData.values.width
-
-        sh =
-            toString graphData.values.height
+        numEdges =
+            graph
+                |> Maybe.map (List.length << .edges)
+                |> Maybe.withDefault 0
     in
     Grid.container []
         [ Grid.row [ Row.attrs [ HA.attribute "class" "py-3" ] ]
             [ Grid.col []
-                [ svg
-                    [ SA.width sw, SA.height sh, SA.viewBox ("0 0 " ++ sw ++ " " ++ sh) ]
-                  <|
-                    viewNodes graphData.nodes
-                        ++ viewEdges graphData.edges
+                [ Html.text <| ("Graph has " ++ toString numNodes ++ " number nodes and " ++ toString numEdges ++ " edges")
                 ]
             ]
-        ]
-
-
-viewNodes : List Node -> List (Svg msg)
-viewNodes nodes =
-    List.map viewNode nodes
-
-
-viewNode : Node -> Svg msg
-viewNode node =
-    let
-        mx =
-            toString <| node.value.x
-
-        my =
-            toString <| node.value.y
-
-        sx =
-            toString <| node.value.x - (toFloat node.value.width / 2.0)
-
-        sy =
-            toString <| node.value.y - (toFloat node.value.height / 2.0)
-
-        sh =
-            toString node.value.height
-
-        sw =
-            toString node.value.width
-    in
-    Svg.node "g"
-        []
-        [ rect
-            [ SA.x sx
-            , SA.y sy
-            , SA.width sw
-            , SA.height sh
-            , SA.rx "10"
-            , SA.ry "10"
-            , SA.stroke "blue"
-            , SA.fill "white"
-            ]
-            []
-        , text_
-            [ SA.x mx
-            , SA.y my
-            , SA.alignmentBaseline "central"
-            , SA.textAnchor "middle"
-            ]
-            [ text node.value.label ]
-        ]
-
-
-viewEdges : List Edge -> List (Svg msg)
-viewEdges edges =
-    List.map viewEdge edges
-
-
-viewEdge : Edge -> Svg msg
-viewEdge edge =
-    let
-        mx =
-            toString edge.values.x
-
-        my =
-            toString edge.values.y
-
-        sx { x } =
-            toString x
-
-        sy { y } =
-            toString y
-
-        p2s p =
-            sx p ++ "," ++ sy p
-
-        pnts =
-            String.join " " <| List.map p2s edge.values.points
-    in
-    node "g"
-        []
-        [ polyline [ SA.fill "none", SA.stroke "black", SA.points pnts ] []
-        , text_
-            [ SA.x mx
-            , SA.y my
-            , SA.alignmentBaseline "central"
-            , SA.textAnchor "middle"
-            ]
-            [ text edge.values.label ]
         ]
 
 
